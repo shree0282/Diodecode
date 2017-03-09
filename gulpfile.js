0e6462fbeefa90c5606969740c4e76adf0bf4e77
Avadesh mishra <Avadesh mishra>
2017-03-09 16:42:54 +0530

    all file commited

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('production/**/*.scss')
+    gulp.src('development/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('default',function() {
-    gulp.watch('production/**/*.scss',['styles']);
+    gulp.watch('development/**/*.scss',['styles']);
 });
---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Khand|Lato|Open+Sans" rel="stylesheet">
+
+	<title>Diodecode</title>
+
+	<script
+	  src="https://code.jquery.com/jquery-3.1.1.min.js"
+	  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+	  crossorigin="anonymous">	
+	</script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="build/style.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">	
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#toggle-menu" aria-expanded="false">
+	                  <span class="icon-bar"></span>
+	                  <span class="icon-bar"></span>
+	                  <span class="icon-bar"></span>
+                </button>
+                 <a href="#"> 
+                    <img src="images/logo.png" width="220"/>
+                    <div class="clear"> </div>
+                </a>
+			</div>
+			 <div class=" collapse navbar-collapse openSans" id="toggle-menu">
+			 
+				<ul class="nav navbar-nav navbar-right ">
+					<li><a href="#">HOME</a></li>
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      SERVICE <span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+
+	                      <li class="dropdown-submenu">
+	                     	 <a href="#">Web Development</a>
+	                     	 	<ul class="dropdown-menu">
+	                     	 		<li><a href="#">Website Designing</li>
+	                     	 		<li><a href="#">Web Application Development</li>
+	                     	 		<li><a href="#">Web Services & APIs Development</a></li>
+	                     	 	</ul>
+	                      </li>
+
+	                      <li class="dropdown-submenu">
+	                     	 <a href="#">Mobile Application Development</a>
+	                     	 	<ul class="dropdown-menu">
+	                     	 		<li><a href="#">Hybrid App Development</li>
+	                     	 		<li><a href="#">Android App Development</li>
+	                     	 		<li><a href="#">IOS App Development</a></li>
+	                     	 	</ul>
+	                      </li>
+
+	                      <li class="dropdown-submenu">
+	                     	 <a href="#">Proffesional Services</a>
+	                      	<ul class="dropdown-menu">
+	                      		<li><a href="#">Managed IT Services</a></li>
+	                      		<li><a href="#">Cloud Server Support</a></li>
+	                      		<li><a href="#">On Demand IT Services</a></li>
+	                      	</ul>	                      
+	                      </li>
+
+                      </ul>
+					</li> <!-- Services end here -->
+								
+
+		<!-- Solution Menu start here -->
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      SOLUTION<span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+
+	                      <li class="dropdown-submenu">
+	                     	 <a href="#">E-Commerce</a>
+	                     	 	<ul class="dropdown-menu">
+	                     	 		<li><a href="#">Magento</li>
+	                     	 		<li><a href="#">WOO Commerce</li>
+	                     	 		<li><a href="#">Custom Made</a></li>
+	                     	 	</ul>
+	                      </li>
+
+	                      <li class="dropdown-submenu">
+	                     	 <a href="#">Content Management System</a>
+	                     	 	<ul class="dropdown-menu">
+	                     	 		<li><a href="#">Wordpress</li>
+	                     	 		<li><a href="#">Drupal</a></li>
+	                     	 	</ul>
+	                      </li>
+
+	                      <li class="dropdown-submenu">
+	                     	 <a href="#">Hosting</a>
+	                      	<ul class="dropdown-menu">
+	                      		<li><a href="#">Shared Hosting</a></li>
+	                      		<li><a href="#">Dedicated Hosting</a></li>
+	                      	</ul>	                      
+	                      </li> <!-- solution end here -->                      
+                      </ul>
+                    <li><a href="#"> PORTFOLIO </a></li>
+                  	<li><a href="#"> CONTACT US </a></li>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	<!-- Footer Start here -->
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<h3>ABOUT DIODECODE</h3>
+				<ul>
+					<li> <a href="#">About Us</a> </li>
+					<li> <a href="#">Contact Us</a> </li>
+					<li> <a href="#">Portfolio</a> </li>
+				</ul>
+			</div>
+		
+			<div class="col-md-3">
+				<h3>PRODUCTS</h3>
+				<ul>
+					<li> <a href="#"> Style Ui  </a> </li>
+             		<li> <a href="#"> Redon  </a> </li>
+             		<li> <a href="#"> Sonic </a> </li>
+             		<li> <a href="#"> Operating System </a> </li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h3>SERVICES</h3>
+				<ul>
+					<li> <a href="#"> Web Development  </a> </li>
+             		<li> <a href="#"> Mobile App Development </a> </li>
+             		<li> <a href="#"> Professional Services </a> </li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<h3>SOLUTIONS</h3>
+				<ul>
+					<li> <a href="#"> Cloud Hosting  </a> </li>
+             		<li> <a href="#"> Content Management System </a> </li>
+             		<li> <a href="#"> E-Commerce </a> </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<footer class="footer1">
+	 	<div class="container">
+	 		Copyrights © 2016 All Rights Reserved by <a href="#"><b>DiodeCode</b>.</a>
+	 	</div>
+</footer>
+
+</body>
+</html>
