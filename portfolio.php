aa41db1ebf9348d512ec33fb488b7656ccede1e9
Avadesh mishra <Avadesh mishra>
2017-03-16 16:58:57 +0530

    All file commited

--- portfolio.php
+++ portfolio.php
@@ -5,7 +5,7 @@ include "inc/header.php";
 <div class="container-fluid" style="margin-top: -20px">
    <div class="row" >    
    		<div style="margin: auto; text-align: center; background: #4EC5BF;  padding: 100px; display: block;  " >
-   			<h1 class="heading" style="color: white;  ">Portfolio</h1>
+   			<h1 class="heading" style="color: white; "> Portfolio </h1>
    		</div>      
    </div>
 </div>
